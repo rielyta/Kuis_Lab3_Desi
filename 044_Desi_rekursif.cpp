b3ce04287757e24c10bd32c94dce1061bb52e88d
rielyta <desi@students.usu.ac.id>
2024-05-01 14:26:56 +0700

    rekursif

---
+++ 044_Desi_rekursif.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int hitungfpb(int a, int b) {
+    if (b==0)
+        return a;
+    else
+        return hitungfpb(b,a%b);
+}
+
+int main() {
+    int m,n;
+    cout<<"m: ";
+    cin>>m;
+    cout<<"n: ";
+    cin>>n;
+    int fpb = hitungfpb(m,n);
+    cout << "FPB: " << fpb << endl;
+}
