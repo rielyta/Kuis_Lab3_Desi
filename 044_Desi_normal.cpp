97ae5a6f328398077afd16c6090bfc07a4382043
rielyta <desi@students.usu.ac.id>
2024-05-02 07:45:01 +0700

    normal komentar

--- 044_Desi_normal.cpp
+++ 044_Desi_normal.cpp
@@ -3,22 +3,23 @@ using namespace std;
 
 
 //fungsi menghitung dengan Algoritma Euclidean
-int hitungfpb (int a, int b) {
-    int r;
-    while(a%b!=0) {
-        r=a%b;
-        a=b;
-        b=r;
+int hitungfpb (int a, int b) { //fungsi hitungfpb dengan parameter a dan b
+    int r;//deklarasi variabel r
+    while(a%b!=0) {//perulangan jika a tidak habis dibagi b
+        r=a%b;//r adalah a mod b
+        a=b;//a menjadi b
+        b=r;//b menjadi r
     }
-    return b;
+    return b;//mengembalikan nilai b
 }
+//looping akan berhenti jika a habis dibagi b, maka nilai b adalah FPB dari a dan b
 
 int main() {
-    int m ,n;
-    cout<<"m: ";
-    cin>>m;
-    cout<<"n: ";
-    cin>>n;
-    int fpb = hitungfpb(m, n);
-    cout << "FPB: " << fpb << endl;
+    int m ,n; 
+    cout<<"m: "; //input m
+    cin>>m;//memasukkan input ke variabel m
+    cout<<"n: ";//input n
+    cin>>n;//memasukkan input ke variabel n
+    int fpb = hitungfpb(m, n);//menghitung fpb dengan memanggil fungsi hitungfpb dengan parameter m dan n
+    cout << "FPB: " << fpb << endl;//output fpb
 }
