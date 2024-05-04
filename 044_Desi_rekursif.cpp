88e738dd6df3f1203dda7795635eab0f32983a70
rielyta <desi@students.usu.ac.id>
2024-05-04 12:09:08 +0700

    rekursif dan komentar

--- 044_Desi_rekursif.cpp
+++ 044_Desi_rekursif.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 //fungsi menghitung fpb dengan Algoritma Euclidean
-int hitungfpb(int a, int b) {
-    if (b==0)
-        return a;
+int hitungfpb(int a, int b) { //fungsi hitungfpb dengan parameter a dan b
+    if (b==0)//jika b=0
+        return a;//maka nilai a adalah FPB dari a dan b
     else
-        return hitungfpb(b,a%b);
+        return hitungfpb(b,a%b);//jika b!=0, maka fungsi hitungfpb dipanggil kembali dengan parameter b dan a mod b
 }
 
 int main() {
     int m,n;
-    cout<<"m: ";
-    cin>>m;
-    cout<<"n: ";
-    cin>>n;
-    int fpb = hitungfpb(m,n);
-    cout << "FPB: " << fpb << endl;
+    cout<<"m: ";//input m
+    cin>>m;//memasukkan input ke variabel m
+    cout<<"n: ";//input n
+    cin>>n;//memasukkan input ke variabel n
+    int fpb = hitungfpb(m,n);//menghitung fpb dengan memanggil fungsi hitungfpb dengan parameter m dan n
+    cout << "FPB: " << fpb << endl;//output fpb
 }
